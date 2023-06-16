ba7d1edda2b816ba1bb013798a263f3f6b0f4a7b
DaeHyun_Woo <po668312@naver.com>
2023-06-16 20:02:18 +0900

    feat: 인터페이스 알아보기

---
+++ class-note/3_interface.ts
@@ -0,0 +1,76 @@
+// daehyun이라는 변수는 User라는 인터페이스를 갖는다라고 해석
+// 반복되는 타입들에 대해서 하나의 인터페이스로 정의를 하고 그 인터페이스를 사용하여 중복된 내용을 제거할 수 있다는 장점이 있다.
+
+interface User {
+  name: string;
+  age: number;
+}
+
+// 변수에 인터페이스 활용
+var daehyun: User = {
+  name: "woodaehyun",
+  age: 30,
+};
+
+// 함수의 매개변수를 정의하는 인터페이스 활용
+function getUser(user: User) {
+  console.log(user);
+}
+
+const userOne = {
+  name: "thor",
+  age: 1002,
+};
+
+getUser(userOne);
+
+// 함수 스펙(구조)에 정의하는 인터페이스 활용
+interface SumFunction {
+  (a: number, b: number): number;
+}
+
+var sum: SumFunction = (a: number, b: number) => {
+  return a + b;
+};
+
+// 인덱싱 방식을 정의하는 인터페이스
+interface StringArr {
+  [index: number]: string;
+}
+
+var stringArray: StringArr = ["a", "b", "c"];
+stringArray[0] = "d";
+
+// 딕셔너리 패턴
+interface StringObj {
+  [key: string]: number;
+}
+
+var StringObject: StringObj = {
+  name: 123,
+  address: 29392,
+};
+
+StringObject["name"] = 1929;
+
+// 인터페이스 확장
+interface Person {
+  name: string;
+  age: number;
+}
+
+// interface Developer {
+//   name: string;
+//   age: number;
+//   language: string;
+// }
+
+interface Developer extends Person {
+  language: string;
+}
+
+var developer: Developer = {
+  language: "typescript",
+  name: "woodaehyun",
+  age: 30,
+};
--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,14 +1,23 @@
-type todo = {
+// 중복 제거를 위해서 따로 만드는 것! 이것을 타입 별칭이라고 한다.
+// {id: number, title: string, done: boolean} 을 Todo라는 이름으로 별칭을 붙인 것이다.
+// 즉 기존에 기본 타입들을 활용하여 Todo라는 타입을 새로 만들어 낸 것이다.
+// type Todo = {
+//   id: number;
+//   title: string;
+//   done: boolean;
+// };
+
+interface Todo {
   id: number;
   title: string;
   done: boolean;
-};
+}
 
-let todoItems: todo[];
+let todoItems: Todo[];
 
 // api
-function fetchTodoItems(): todo[] {
-  const todos: todo[] = [
+function fetchTodoItems(): Todo[] {
+  const todos: Todo[] = [
     { id: 1, title: "안녕", done: false },
     { id: 2, title: "타입", done: false },
     { id: 3, title: "스크립트", done: false },
@@ -17,12 +26,12 @@ function fetchTodoItems(): todo[] {
 }
 
 // crud methods
-function fetchTodos(): todo[] {
-  const todos: todo[] = fetchTodoItems();
+function fetchTodos(): Todo[] {
+  const todos: Todo[] = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo: todo): void {
+function addTodo(todo: Todo): void {
   todoItems.push(todo);
 }
 
@@ -30,23 +39,23 @@ function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index: number, todo: todo) {
+function completeTodo(index: number, todo: Todo) {
   //여기서 todo의 타입을 object로 지정하게 되면 object 타입에 done 프로퍼티가 존재한다는 것을 보장할 수가 없어서 에러가 발생함!
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo(): todo {
+function logFirstTodo(): Todo {
   return todoItems[0];
 }
 
-function showCompleted(): todo[] {
+function showCompleted(): Todo[] {
   return todoItems.filter((item) => item.done);
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems(todo1?: todo, todo2?: todo): void {
+function addTwoTodoItems(todo1?: Todo, todo2?: Todo): void {
   todo1 && addTodo(todo1);
   todo2 && addTodo(todo2);
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
